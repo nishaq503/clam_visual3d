f75ddd4385c8e69b365c2c06b8d7a93ae2465726
djperrone <david_perrone@uri.edu>
2023-06-27 12:49:53 -0400

    updated to latest clam/cakes

--- unity/Assets/Scripts/ClamFFI.cs
+++ unity/Assets/Scripts/ClamFFI.cs
@@ -12,7 +12,7 @@ namespace ClamFFI
 
     public static partial class Clam
     {
-	public const string __DllName = "clam_ffi_20230627121251";
+	public const string __DllName = "clam_ffi_20230627124016";
         private static IntPtr _handle;
 
         [DllImport(__DllName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "free_string")]
